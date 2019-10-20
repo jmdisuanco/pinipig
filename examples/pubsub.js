ff9e3b27a3fb78c67301c07ca9cec448078cee0a
JM <jm@disuan.co>
2019-10-20 12:43:25 +0400

    PubSub demo

--- examples/pubsub.js
+++ examples/pubsub.js
@@ -5,23 +5,9 @@
 
 const pinipig = require('../pinipig')
 
-const Handshake = (ctx) => {
-    ctx.ws.send(`Connected via url${ctx.req.getUrl()}`)
-}
 
 const Subscribe = (ctx) => {
     ctx.ws.subscribe('chat/#');
-}
-
-const WSMessage = (ctx) => {
-    try{
-        let  data = Buffer.from(ctx.message).toString("binary")
-        console.log(ctx.ws.getRemoteAddress())
-         let ok = ctx.ws.send(data.toUpperCase(), ctx.isBinary)
-    }
-  catch(e){
-    console.log(e)
-  }
 }
 
 const Publish = (ctx) => {
